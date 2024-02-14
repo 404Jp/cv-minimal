1f2f1062dd3e7784bcfd6578db82d54c806529ed
Rip-23 <113641017+Rip-23@users.noreply.github.com>
2024-02-14 20:17:48 -0300

    up

---
+++ src/data/info.ts
@@ -0,0 +1,203 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Jean Bouffanais",
+  jobDescription: "Web Developer",
+  about: ` `, 
+
+  hero: [
+    {
+      name: "Jean P. Bouffanais",
+      location: "Santiago, Chile 🌎",
+      description: [
+        "¡Bienvenido a mi portafolio!",
+        "Aquí encontrarás mi información más actualizada y una visión de mi experiencia y habilidades.",
+        "Si tienes alguna pregunta o consulta, no dudes en contactarme.",
+      ],
+    },
+  ],
+
+  experience: [
+    
+    {
+      name: "Full Stack Developer (Part-Time)",
+      place: "Freelance por Proyecto",
+      location: "Remoto",
+      startDate: "Sep 2023",
+      endDate: "Dic 2023",
+      description: [
+        "✓ Desarrollo de Websites estáticos con Astro JS y React",
+        "✓ Creación de Landing Page para uso de portafolio",
+        "✓ Vercel despliegue, gestión de sitios web con Astro JS",
+      ],
+    },
+    {
+      name: "Práctica Profesional Unidad de Exploración de Sistemas (Full-Time)",
+      place: "Ministerio de Medio Ambiente - SEA",
+      location: "Calle Miraflores 222, Santiago",
+      startDate: "Mar 2023",
+      endDate: "May 2023",
+      description: [
+        "✓ Programación de páginas y módulos en PHP POO",
+        "✓ Atención de tickets por medio de Jira Service Desk",
+        "✓ Ayuda y capacitación a otros colegas",
+        "✓ Consultas y Modificación de base de datos con Azure Data Studio",
+        "✓ Metodología Scrum",
+      ],
+    },
+    {
+      name: "Especialista en Hardware (Part-Time)",
+      place: "Universidad Tecnológica Metropolitana",
+      location: "José Pedro Alessandri 1242, Ñuñoa",
+      startDate: "Sep 2022",
+      endDate: "Dic 2022",
+      description: [
+        "Encargado del laboratorio de informática en reemplazo de colaborador durante un semestre.",
+        "✓ Mantenimiento de Hardware de los laboratorios.",
+        "✓ Atención de estudiantes y académicos de la Universidad.",
+        "✓ Preparación de material para clases de informática en laboratorios.",
+      ],
+    },
+    {
+      name: "Armado y Mantenimiento de Hardware de minería (Full-Time)",
+      place: "Freelance",
+      location: "",
+      startDate: "Jun 2020",
+      endDate: "May 2021",
+      description: [
+        "✓ Armado de Rig's de minería de Ethereum con tarjetas gráficas Nvidia y AMD.",
+        "✓ Mantenimiento de Rig's de minería",
+        "✓ Configuración de Pool de minería Hiveon",
+        "✓ Configuración de Wallet de criptomonedas",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "DEFENSIVE SECURITY ANALYST - HTB",
+      location: "academy.hackthebox.com",
+      startDate: "Feb 2024",
+      endDate: "Actual",
+      description: [
+        "Procesos y metodologías de SOC",
+        "Operaciones SIEM (ELK/Splunk)",
+        "Analítica táctica",
+        "Análisis de registros",
+        "Búsqueda de amenazas",
+        "Análisis de ataques de Active Directory",
+        "Análisis de tráfico de red (incl. IDS/IPS)",
+        "Análisis de malware",
+        "Operaciones DFIR"
+      ],
+    },
+    {
+      name: "Fundamentos de Ciberseguridad un enfoque práctico - UC3M",
+      location: "Remoto por Edx",
+      startDate: "Nov 2023",
+      endDate: "Ene 2024",
+      description: [
+        "Curso con enfoque práctico de la Universidad Carlos III de Madrid.",
+        "Contenido:",
+        "Informática forense",
+        "Ingeniería inversa",
+        "Ciberdefensa",
+        "Malware y Amenazas Persistentes Avanzadas (APTs)",
+        "Vulnerabilidades y exposiciones",
+      ],
+    },
+    {
+      name: "Ingeniería en Informática - UTEM",
+      location: "José Pedro Alessandri 1242, Ñuñoa",
+      startDate: "Mar 2018",
+      endDate: "Ago 2023",
+      description: [
+        "Grado de ingeniería en informática de la Universidad Tecnológica Metropolitana",
+        "Lenguajes de programación C / C++",
+        "Cursos de especialización:",
+        "Auditoría de sistemas",
+        "Tópicos de ciberseguridad",
+        "Desarrollo móvil (React Native & Firebase)",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/",
+    twitter: "https://twitter.com/",
+    github: "https://github.com/Jp-404bot",
+    email: "mailto:jeanpierre.bouffanais@gmail.com",
+    linkedin: "https://www.linkedin.com/in/jean-p-bouffanais",
+  },
+
+  projects: [
+    {
+      title: "Elastic Stack with Endpoint Linux.",
+      isFeatured: true,
+      thumbnail: "/assets/certification/dataetl.png",
+      liveUrl: "https://www.elastic.co/es/security/siem",
+    },
+    {
+      title: "Cybersecurity Job Simulation - JPMorgan Chase&Co.",
+      isFeatured: true,
+      thumbnail: "/assets/certification/3312.png",
+      githubUrl: "https://github.com/Jp-404bot/Spam-Email-Classifier-using-Logistic-Regression",
+    },
+    {
+      title: "Data Science Job Simulation - British Airways",
+      isFeatured: true,
+      thumbnail: "/assets/certification/British-Jp.png",
+      githubUrl: "https://github.com/Jp-404bot/Data-Web-scraping",
+    },
+    {
+      title: "Cybersecurity Job Simulation - Telstra",
+      isFeatured: true,
+      thumbnail: "/assets/certification/tesltra-JP.png",
+      liveUrl: "https://www.theforage.com/simulations/telstra/cybersecurity-cyyo",
+    },
+    {
+      title: "Proyecto plataforma web en Laravel 8 (Proyecto de titulación)",
+      isFeatured: true,
+      thumbnail: "/assets/certification/laravel-8.png",
+      githubUrl: "https://github.com/Jp-404bot/Laravel-8",
+    },
+    {
+      title: "Aplicación móvil con React Native",
+      isFeatured: true,
+      thumbnail: "/assets/certification/app-movil.png",
+      githubUrl: "https://github.com/Jp-404bot/ReactNative-Git",
+    },
+  ],
+
+  certification: [
+    {
+      title: "Auditor Interno 27001:2013 - CertiProf",
+      isFeatured: true,
+      thumbnail: "/assets/certification/auditor.png",
+    },
+    {
+      title: "Carrera Seguridad Informática - Platzi",
+      isFeatured: true,
+      thumbnail: "/assets/certification/seguridad informatica.png",
+    },
+    {
+      title: "Cloud Computing Avanzado para TI",
+      isFeatured: true,
+      thumbnail: "/assets/certification/1676199149933.png",
+    },
+    {
+      title: "Udemy Curso Completo",
+      isFeatured: true,
+      thumbnail: "/assets/certification/UC-10a54f43-cde8-4591-b59e-59b878192458.jpg",
+    },
+    {
+      title: "Regresión lineal con Python",
+      isFeatured: true,
+      thumbnail: "/assets/certification/lineral.jpeg",
+    },
+    {
+      title: "Comunicación con satélites",
+      isFeatured: true,
+      thumbnail: "/assets/certification/satellite.jpeg",
+    },
+  ],
+};
